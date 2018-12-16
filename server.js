10d1f47fd2c0fc9528e1fd987f043d5e18c79991
megido_ <megido666@gmail.com>
2018-12-16 20:58:22 +0200

    heroku update

---
+++ server.js
@@ -0,0 +1,15 @@
+constexpress=require('express');
+constapp=expres();
+constpath=require('path');
+
+app.us(express.static(__dirname+'/dist'));
+
+app.listen(process.env.PORT||8080);
+
+
+//PathLocationStradegy
+app.get('/'function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+console.log('Console Listening'); 
